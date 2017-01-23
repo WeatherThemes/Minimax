f17b5823a20d3402fd161006e2f97804551b7aeb
Spandan <dhungelspandan@gmail.com>
2017-01-23 14:23:59 +0545

    all files added

---
+++ footer.php
@@ -0,0 +1,64 @@
+<!-- footer section -->
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-sm-6">
+				<h2>Our Office</h2>
+				<p>101 Clean Street, California, CA 10110</p>
+				<p>Email: <span>hello@company.com</span></p>
+				<p>Phone: <span>010-020-0340</span></p>
+			</div>
+			<div class="col-md-6 col-sm-6">
+				<h2>Social Us</h2>
+				<ul class="social-icons">
+					<li><a href="#" class="fa fa-facebook"></a></li>
+					<li><a href="#" class="fa fa-twitter"></a></li>
+                    <li><a href="#" class="fa fa-google-plus"></a></li>
+					<li><a href="#" class="fa fa-dribbble"></a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<?php wp_footer(); ?>
+</footer>
+
+<!-- divider section -->
+<div class="container">
+	<div class="row">
+		<div class="col-md-1 col-sm-1"></div>
+		<div class="col-md-10 col-sm-10">
+			<hr>
+		</div>
+		<div class="col-md-1 col-sm-1"></div>
+	</div>
+</div>
+
+<!-- copyright section -->
+<div class="copyright">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-sm-12">
+				<p>Copyright &copy; 2016 Minimax Digital Firm 
+                
+                - Design: <a rel="nofollow" href="http://www.tooplate.com" target="_parent">Tooplate</a></p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- scrolltop section -->
+<a href="#top" class="go-top"><i class="fa fa-angle-up"></i></a>
+
+
+<!-- javascript js -->	
+<script src="<?php bloginfo('template_directory'); ?>/js/jquery.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/bootstrap.min.js"></script>	
+<script src="<?php bloginfo('template_directory'); ?>/js/nivo-lightbox.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/smoothscroll.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/jquery.nav.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/isotope.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/imagesloaded.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/custom.js"></script>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+
+	<title>Minimax HTML5 Free Template</title>
+<!--
+
+Template 2080 Minimax
+
+http://www.tooplate.com/view/2080-minimax
+
+-->
+	<!-- stylesheet css -->
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/font-awesome.min.css">
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/nivo-lightbox.css">
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/nivo_themes/default/default.css">
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/style.css">
+	<!-- google web font css -->
+	<link href='http://fonts.googleapis.com/css?family=Raleway:400,300,600,700' rel='stylesheet' type='text/css'>
+<?php wp_head(); ?>
+</head>
